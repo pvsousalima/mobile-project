9ecafd30f8b92b53c28f1f6e6d088843d680e86d
Pedro Victor <pvsousalima@gmail.com>
2015-05-04 16:16:09 -0500

    Alteracoes feitas

--- app/src/main/java/com/example/msu/monstersfactory/MainActivity.java
+++ app/src/main/java/com/example/msu/monstersfactory/MainActivity.java
@@ -11,29 +11,58 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+    //SharedPreferences
     final String PREFS_NAME = "MyPrefsFile";
+    SharedPreferences settings;
 
-    SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
+    public boolean checkFirstLaunch(){
         if (settings.getBoolean("my_first_time", true)) {
-
-            //the app is being launched for first time, do something
-            Log.d("Comments", "First time");
 
             // first time task
             Toast t = Toast.makeText(getApplicationContext(), "First time", Toast.LENGTH_LONG);
             t.show();
 
             // record the fact that the app has been started at least once
-            settings.edit().putBoolean("my_first_time", false);
+            settings.edit().putBoolean("my_first_time", false).apply();
             settings.edit().commit();
+
+            return true;
+        } else {
+
+            // Checks the second time launch
+            //the app is being launched for second time, do something
+
+            setContentView(R.layout.second);
+
+            // first time task
+            Toast t = Toast.makeText(getApplicationContext(), "Second time", Toast.LENGTH_LONG);
+            t.show();
+
+            return false;
         }
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Get SharedPreferences
+        settings = getSharedPreferences(PREFS_NAME, 0);
+
+
+        if (checkFirstLaunch()==true){
+            setContentView(R.layout.activity_main);
+
+        }else{
+            setContentView(R.layout.second);
+
+        }
+
 
 
 
