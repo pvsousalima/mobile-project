b3172fbd4d0b98b323ad085b5706f18f5cd117c5
Pedro Victor <pvsousalima@gmail.com>
2015-05-04 23:53:18 -0500

    Tela inicial finalizada

---
+++ app/src/main/java/com/example/msu/monstersfactory/FormActivity.java
@@ -0,0 +1,23 @@
+package com.example.msu.monstersfactory;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by pvsousalima on 5/4/15.
+ */
+public class FormActivity extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.second);
+
+
+
+    }
+}
+
+
--- app/src/main/java/com/example/msu/monstersfactory/MainActivity.java
+++ app/src/main/java/com/example/msu/monstersfactory/MainActivity.java
@@ -1,12 +1,24 @@
 package com.example.msu.monstersfactory;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.io.InputStream;
+import java.net.URL;
 
 
 public class MainActivity extends Activity {
@@ -16,7 +28,7 @@ public class MainActivity extends Activity {
     SharedPreferences settings;
 
 
-
+    ImageView imageView;
 
     public boolean checkFirstLaunch(){
         if (settings.getBoolean("my_first_time", true)) {
@@ -45,26 +57,39 @@ public class MainActivity extends Activity {
         }
     }
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+
         //Get SharedPreferences
         settings = getSharedPreferences(PREFS_NAME, 0);
 
+        //Gets ImageView object
+        imageView = (ImageView) findViewById(R.id.image);
 
-        if (checkFirstLaunch()==true){
-            setContentView(R.layout.activity_main);
-            System.out.println("Railan");
+        // Loads image from the Internet
+        Picasso.with(getApplicationContext())
+        .load("https://scontent.xx.fbcdn.net/hphotos-xfp1/t31.0-8/1798924_795706623828627_1556304660425544635_o.png")
+        .into(imageView);
 
-        }else{
-            setContentView(R.layout.second);
-
-        }
+        Toast t = Toast.makeText(getApplicationContext(), "Load image", Toast.LENGTH_SHORT);
+        t.show();
 
 
+        Button button_new = (Button) findViewById(R.id.button_new);
+
+
+        button_new.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i  = new Intent(getApplicationContext(), FormActivity.class);
+
+                startActivity(i);
+            }
+        });
 
 
     }
