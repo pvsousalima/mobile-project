0c60cd2fa27c4ad9caaed904d2fcebcbe6340138
Pedro Victor <pvsousalima@gmail.com>
2015-05-12 01:07:48 -0500

    Update on BMI Activity

--- app/src/main/java/com/example/msu/monstersfactory/BMI.java
+++ app/src/main/java/com/example/msu/monstersfactory/BMI.java
@@ -1,9 +1,12 @@
 package com.example.msu.monstersfactory;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class BMI extends Activity {
@@ -12,7 +15,6 @@ public class BMI extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
-
 
 
 
--- app/src/main/java/com/example/msu/monstersfactory/ObjectivesActivity.java
+++ app/src/main/java/com/example/msu/monstersfactory/ObjectivesActivity.java
@@ -1,9 +1,12 @@
 package com.example.msu.monstersfactory;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class ObjectivesActivity extends Activity {
@@ -12,6 +15,17 @@ public class ObjectivesActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_objectives);
+
+        Button next = (Button) findViewById(R.id.button_next);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), BMI.class);
+                startActivity(i);
+            }
+        });
+
     }
 
     @Override
