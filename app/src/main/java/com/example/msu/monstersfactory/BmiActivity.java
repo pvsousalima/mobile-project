ea8765b7dc34e3da856e76e68f63396492913703
Pedro Victor <pvsousalima@gmail.com>
2015-05-13 01:03:17 -0500

    Finished BMI screen

--- app/src/main/java/com/example/msu/monstersfactory/BmiActivity.java
+++ app/src/main/java/com/example/msu/monstersfactory/BmiActivity.java
@@ -1,23 +1,17 @@
 package com.example.msu.monstersfactory;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 
-public class BMI extends Activity {
+public class BmiActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bmi);
-
-
-
     }
 
     @Override
